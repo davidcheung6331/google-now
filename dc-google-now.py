1f5a65d6cb6ce65c1b2bfab1449c0e86872ae9d7
david cheung <107241896+davidcheung6331@users.noreply.github.com>
2023-07-06 11:39:15 +0800

    remove the subProcess audio playback

--- dc-google-now.py
+++ dc-google-now.py
@@ -4,8 +4,6 @@ from langchain.llms import OpenAI
 from langchain.chains import ConversationChain
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
-import subprocess
-import speech_recognition as sr
 from PIL import Image
 image = Image.open("search2.jpg")
 
@@ -59,57 +57,15 @@ agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbos
 
 
 SearchStr = "what is the today biggest news from 10  years ago ?"
-recorded_text = ""
-
-if st.button("Start Recording"):
-    r = sr.Recognizer()
-    mic = sr.Microphone()
-
-    try:
-        with mic as source:
-            r.adjust_for_ambient_noise(source)
-            st.info("Recording started...")
-
-            audio = r.listen(source, phrase_time_limit=10)
-            transcript = r.recognize_google(audio)
-            st.info(transcript)
-
-            # Update the value of SearchStr with the transcribed text
-            recorded_text = transcript
-            print(f"1. recorded_text = {recorded_text}")
-
-
-    except sr.UnknownValueError:
-        st.warning("No speech detected.")
-    except sr.RequestError as e:
-        st.error(f"Error: {e}")
-
-# Update the value of SearchStr with the recorded_text
-SearchStr = recorded_text if recorded_text else SearchStr
-print(f"1. SearchStr = {SearchStr}")
-            
 
 
 # Display the updated value of SearchStr in the text_input control
-recorded_text = st.text_input(":point_right:", value=SearchStr)
-print(f"2. recorded_text = {recorded_text}")
+SearchStr = st.text_input(":point_right:", value=SearchStr)
 
-
-# if st.button("conversation Search"):
-#     print(f"3. recorded_text = {recorded_text}")
-#     SearchResult = conversation.predict(input=recorded_text)
-#     st.info(SearchResult)
-#     # text to spech the search
-#     # espeak(SearchResult)
-#     subprocess.call(["say", SearchResult])
 
 if st.button("Search"):
-    print(f"3. recorded_text = {recorded_text}")
-    SearchResult = agent.run(recorded_text)
+    SearchResult = agent.run(SearchStr)
     st.info(SearchResult)
-    # text to spech the search
-    # espeak(SearchResult)
-    subprocess.call(["say", SearchResult])
 
 
 log = """
